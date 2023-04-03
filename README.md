1550a0cc939dc50f11e71ee86fbd8b05c1439b10
jaroslav rodny <129297971+onlyradiobutnohead@users.noreply.github.com>
2023-04-03 13:49:42 +0200

    readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# sekoittaa
+
+Taken from the finnish word sekoittaa, meaning 'to mix'. This script
+shuffles up your wav file data to provide an interesting randomised
+aspect to your project.
+
+## Usage
+
+`perl sekoittaa.pl <wav_file> [output]`
+
+If `output` is not provided, sekoittaa will output your file with the
+suffix `-sekoitted`.
--- sekoittaa.pl
+++ sekoittaa.pl
@@ -1,38 +1,46 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
 use File::Basename;
+use List::Util qw/shuffle/;
 
 if ($#ARGV < 0) {
-    print "Usage: sekoittaa.pl <input wav|flac|mp3> [output]\n";
+    print "Usage: sekoittaa.pl <wav_file> [output]\n";
     exit 1;
 }
 
 my $input_file = $ARGV[0];
-my ($name, $path, $ext) = fileparse($input_file);
-my $output_file = $#ARGV ? $ARGV[1] : "$name-sekoitted.$ext";
+my ($name, $path, $ext) = fileparse($input_file, qr/\.[^.]*/);
 
 my $contents;
-open(my $in, '<', $input_file) 
+open(my $in, '<:raw', $input_file) 
     or die "Could not open file $input_file\n";
-{
-    local $/;
-    $contents = <$in>;
-}
+open(my $out, '>:raw', $#ARGV ? $ARGV[1] : "$name-sekoitted$ext")
+    or die "Couldn't open output file: $!\n";
 
 # identify file type
-my $magic = substr($contents, 0, 12);
+read($in, my $magic, 12);
 if ($magic =~ m/RIFF[\x00-\xff]{4}WAVE/) {
-    print "wav";
-} elsif ($magic =~ m/\xff[\xf2\xf3\xfb]|ID3/) {
-    print "mp3";
-} elsif ($magic =~ m/fLaC/) {
-    print "flac";
+    print $out $magic;
+    # read till 'data' chunk
+
+    while (read($in, my $chknmsz, 8)) { # handle each chunk
+        print $out $chknmsz;
+        my $chksz_u = unpack("%L", substr($chknmsz, 4, 4));
+        read($in, my $chkdt, $chksz_u);
+
+        if (substr($chknmsz, 0, 4) eq "data") {
+            $chkdt = shuffle $chkdt;
+        }
+
+        print $out $chkdt;
+    }
 } else {
-    print "invalid file type";
+    print "Invalid file type\n";
     exit 1;
 }
 
 close($in);
+close($out);
